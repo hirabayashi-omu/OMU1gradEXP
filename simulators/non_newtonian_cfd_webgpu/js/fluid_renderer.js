17e0db2d076725ae20d86ff97010b11e76a725b3
hirabayashi <hirabayashi@example.com>
2026-09-06 03:31:05 +0900

    fix: fluid_renderer.js構文エラーの完全解消 & v136更新

--- simulators/non_newtonian_cfd_webgpu/js/fluid_renderer.js
+++ simulators/non_newtonian_cfd_webgpu/js/fluid_renderer.js
@@ -2013,7 +2013,6 @@ export class FluidRenderer {
       ctx.stroke();
 
       ctx.restore();
-    }
     } else {
       // 🗡️ B. ドクターブレード本体 (SUS316L 精密研削ブレード)
       const gradBlade = ctx.createLinearGradient(bx - bladeW, 0, bx, 0);
@@ -2859,7 +2858,6 @@ export class FluidRenderer {
       ctx.stroke();
 
       ctx.restore();
-    }
     } else {
       // 🗡️ ドクターブレード刃先 (SUS研磨ブレード拡大)
       const bladeW = 14.0;
