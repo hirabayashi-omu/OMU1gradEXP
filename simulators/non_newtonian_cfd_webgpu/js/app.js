db9a6474d893d97041d31b39d6c9c4fa8deb547c
hirabayashi-omu <hirabayashi@omu.ac.jp>
2026-09-05 14:37:48 +0900

    fix: ツールバーTipsテキストの狭小画面での縦潰れ崩れを防止しレスポンシブ自動非表示・アイコン化を実装

--- simulators/non_newtonian_cfd_webgpu/js/app.js
+++ simulators/non_newtonian_cfd_webgpu/js/app.js
@@ -1,8 +1,8 @@
-import { COSMETIC_PRESETS, RheologyModel, MATERIAL_PALETTES } from './models.js?v=floating_charts_v73';
-import { WebGPUSPHSolver, CONTAINER_TYPES } from './sph_solver_webgpu.js?v=floating_charts_v73';
-import { FluidRenderer } from './fluid_renderer.js?v=floating_charts_v73';
-import { ChartRenderer } from './charts.js?v=floating_charts_v73';
-import { PresetManager } from './preset_manager.js?v=floating_charts_v73';
+import { COSMETIC_PRESETS, RheologyModel, MATERIAL_PALETTES } from './models.js?v=floating_charts_v74';
+import { WebGPUSPHSolver, CONTAINER_TYPES } from './sph_solver_webgpu.js?v=floating_charts_v74';
+import { FluidRenderer } from './fluid_renderer.js?v=floating_charts_v74';
+import { ChartRenderer } from './charts.js?v=floating_charts_v74';
+import { PresetManager } from './preset_manager.js?v=floating_charts_v74';
 
 class CosmeticFillingApp {
   constructor() {
